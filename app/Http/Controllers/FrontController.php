719456327e5e6c365e96e8cc7d993c74d48b5128
jmejiarojas <julioedgarmejiarojas338@gmail.com>
2017-01-10 08:18:42 -0500

    Agrega FrontController y agrega plantilla a las vistas

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function contacto()
+    {
+        return view('contacto');
+    }
+
+    public function reviews()
+    {
+        return view('reviews');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
-
-Route::resource('movie','MovieController');
-/*
-Route::get('/prueba','PruebaController@index');
-Route::get('/name/{nombre}','PruebaController@nombre');
-
-Route::get('cursos/{nombre}', function($nombre){
-    return "Tu nombre es $nombre";
-});
-
-Route::get('edad/{edad?}', function($edad = 20){
-    return "Su edad es $edad";
-});
-*/
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', 'FrontController@home');
+Route::get('/contacto', 'FrontController@contacto');
+Route::get('/reviews', 'FrontController@reviews');
